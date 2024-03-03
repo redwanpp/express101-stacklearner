6e23b14cbebbc48c925f8f218423063ce48220d9
redwanpp <smredwan.info@gmail.com>
2024-03-03 16:49:02 +0600

    gather basic idea about express, app and middleware

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const { log } = require('console');
-const express = require('express')
-const fs = require('fs')
+const express = require('express');
+const fs = require('fs');
+const morgan = require('morgan');
+const cors = require('cors');
 
-const app = express()
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true }));
+app.use(morgan('dev'));
+app.use(cors());
+app.use(globalMiddleware)
 
 app.get('/', (req, res) => {
     fs.readFile('./pages/index.html', (err, data) => {
@@ -16,7 +24,8 @@ app.get('/', (req, res) => {
     });
 });
 
-app.get('/about', (req, res) => {
+
+app.get('/about', localMiddleware, (req, res) => {
     fs.readFile('./pages/about.html', (err, data) => {
         if(err) {
             console.log('Error', err);
@@ -35,11 +44,48 @@ app.get('/help', (req, res) => {
             res.send('Something went wrong');
         } else {
             res.write(data);
-            res.end();
-        }
+            res.end();        }
     })
 })
 
 app.listen(4000, () => {
     console.log('Server is listening on PORT 4000');
 })
+
+
+
+
+/**
+ * 1. This is a middleware signature
+ * 2. if everything seems ok controller will call response methods
+ * 3. if everything seems OK middleware will call next
+ */
+function handler(req, res, next) {
+    /**
+     * read request object
+     * process request
+     * response back the result
+     */
+}
+
+function middlewareSignature(req, res, next) {
+    next();
+}
+
+function globalMiddleware(req, res, next) {
+    console.log(`${req.method} - ${req.url}`);
+    console.log('I am a global middleware');
+
+    if(req.query.bad) {
+        return res.status(400).send('Bad Request');
+    }
+
+    next();
+}
+
+function localMiddleware(req, res, next) {
+    console.log('I am local middleware');
+    next();
+}
+
+// GET, POST, PUT, PATCH, DELETE
