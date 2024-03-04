ac702c6a78382878c5cede9e2a65649a55f13341
redwanpp <smredwan.info@gmail.com>
2024-03-04 09:28:54 +0600

    Handle error globally

--- index.js
+++ index.js
@@ -12,6 +12,20 @@ app.use(cors());
 app.use(globalMiddleware);
 app.use(require('./routes'));
 
+app.use((req, res, next) => {
+    const error = new Error('404 Not Found');
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    console.log('Error', error);
+    if(error.status) {
+        return res.status(error.status).send(`<h1>${error.message}</h1>`);
+    }
+
+    return res.status(500).send('<h1>Somethind went wrong</h1>');
+})
 
 
 app.listen(4000, () => {
